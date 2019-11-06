8e7151b47bee2c70a884dee7eeda476d39f431fc
feschmidt <felix.e.schmidt@gmail.com>
2019-11-06 12:36:37 +0100

    lots of changes: Duffing, notebooks, references, ...

--- compile.sh
+++ compile.sh
@@ -1,4 +1,11 @@
 pdflatex --interaction=batchmode dissertation
 bibtex dissertation
-bibtex dissertation
+bibtex chapter-introduction/introduction
+bibtex chapter-theory/theory
+bibtex chapter-experimental-methods/experiment
+bibtex chapter-gJJ/gJJ
+bibtex chapter-MoRe/moredna
+bibtex chapter-AlJJ/AlJJ
+bibtex chapter-currentdetection/currentdetection
+pdflatex --interaction=batchmode dissertation
 pdflatex --interaction=batchmode dissertation
