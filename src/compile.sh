bad40181ce31066e552ce7dc2e2c376c9b8ef098
feschmidt <felix.e.schmidt@gmail.com>
2020-03-04 04:26:31 +0100

    gJJ-CPR start writing

--- src/compile.sh
+++ src/compile.sh
@@ -5,8 +5,7 @@ bibtex chapter-introduction/introduction
 bibtex chapter-theory/theory
 bibtex chapter-experimental-methods/experiment
 bibtex chapter-gJJ/gJJ
-bibtex chapter-MoRe/moredna
-bibtex chapter-AlJJ/AlJJ
+bibtex chapter-gJJ-CPR/gJJ-CPR
 bibtex chapter-currentdetection/currentdetection
 pdflatex --interaction=batchmode dissertation
 pdflatex --interaction=batchmode dissertation
