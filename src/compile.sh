c49143e25c0913b242f5440882ee227d5c3a51b5
feschmidt <felix.e.schmidt@gmail.com>
2020-03-16 18:22:34 +0100

    lots of updates

--- src/compile.sh
+++ src/compile.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 pdflatex --interaction=batchmode dissertation
-bibtex dissertation
-bibtex chapter-introduction/introduction
-bibtex chapter-theory/theory
-bibtex chapter-experimental-methods/experiment
-bibtex chapter-gJJ/gJJ
-bibtex chapter-gJJ-CPR/gJJ-CPR
-bibtex chapter-currentdetection/currentdetection
+bash src/dobib.sh
 pdflatex --interaction=batchmode dissertation
 pdflatex --interaction=batchmode dissertation
---
+++ src/dobib.sh
@@ -0,0 +1,7 @@
+bibtex dissertation
+bibtex chapter-introduction/introduction
+bibtex chapter-theory/theory
+bibtex chapter-experimental-methods/experiment
+bibtex chapter-gJJ/gJJ
+bibtex chapter-gJJ-CPR/gJJ-CPR
+bibtex chapter-currentdetection/currentdetection
